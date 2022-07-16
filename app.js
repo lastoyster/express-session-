b707bc72175a1bd9eb4ccceea91a90c121289c65
Pratiksha <95347099+lastoyster@users.noreply.github.com>
2022-07-16 19:00:25 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const cookieParser = require("cookie-parser");
+const sessions = require('express-session');
+const app = express();
+const PORT = 4000;
+
+// creating 24 hours from milliseconds
+const oneDay = 1000 * 60 * 60 * 24;
+
+//session middleware
+app.use(sessions({
+    secret: "thisismysecrctekeyfhrgfgrfrty84fwir767",
+    saveUninitialized:true,
+    cookie: { maxAge: oneDay },
+    resave: false
+}));
+
+/ cookie parser middleware
+app.use(cookieParser());
+
+app.get('/',(req,res) => {
+    session=req.session;
+    if(session.userid){
+        res.send("Welcome User <a href=\'/logout'>click to logout</a>");
+    }else
+    res.sendFile('views/index.html',{root:__dirname})
+});
+
+app.post('/user',(req,res) => {
+    if(req.body.username == myusername && req.body.password == mypassword){
+        session=req.session;
+        session.userid=req.body.username;
+        console.log(req.session)
+        res.send(`Hey there, welcome <a href=\'/logout'>click to logout</a>`);
+    }
+    else{
+        res.send('Invalid username or password');
+    }
+})
+
+app.get('/logout',(req,res) => {
+    req.session.destroy();
+    res.redirect('/');
+});
+
+app.listen(PORT, () => console.log(`Server Running at port ${PORT}`));
